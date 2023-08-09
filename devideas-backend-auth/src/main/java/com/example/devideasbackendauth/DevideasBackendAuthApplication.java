a0fe71f5e4c9ef6f2b0c35992f4fb46ba82b77f6
Luigi <lvismara@lapsystec.com>
2023-08-09 12:59:47 -0500

    init repo

---
+++ devideas-backend-auth/src/main/java/com/example/devideasbackendauth/DevideasBackendAuthApplication.java
@@ -0,0 +1,12 @@
+package com.example.devideasbackendauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevideasBackendAuthApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DevideasBackendAuthApplication.class, args);
+  }
+}
---
+++ devideas-backend-auth/src/test/java/com/example/devideasbackendauth/DevideasBackendAuthApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.devideasbackendauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevideasBackendAuthApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
